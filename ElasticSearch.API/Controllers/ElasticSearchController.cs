145cd36b9dfb6c26202a30754701983e5a733e86
furkankocer <furkankocer@hotmail.com.tr>
2020-01-30 21:38:19 +0300

    uploadExcel eklendi

--- ElasticSearch.API/Controllers/ElasticSearchController.cs
+++ ElasticSearch.API/Controllers/ElasticSearchController.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using ElasticSearch.Bussiness;
 using ElasticSearch.Dto;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Nest;
+using OfficeOpenXml;
 
 namespace ElasticSearch.API.Controllers
 {
@@ -48,8 +51,49 @@ namespace ElasticSearch.API.Controllers
                     .InitializeUsing(new IndexState() { Settings = indexSettings })
                     .Aliases(x => x.Alias(indexName));
             }
-
+            model.DateTime = DateTime.Now;
             _elasticClient.Index<LogDto>(model, idx => idx.Index(indexName));
+        }
+
+        [HttpPost("uploadExcel")]
+        public async Task<ExcelResponseDto<List<LogDto>>> Import(IFormFile formFile, CancellationToken cancellationToken)
+        { 
+            if (formFile == null || formFile.Length <= 0)
+            {
+                return ExcelResponseDto<List<LogDto>>.GetResult(-1, "formfile is empty");
+            }
+
+            if (!Path.GetExtension(formFile.FileName).Equals(".xlsx", StringComparison.OrdinalIgnoreCase))
+            {
+                return ExcelResponseDto<List<LogDto>>.GetResult(-1, "Not Support file extension");
+            }
+
+            var list = new List<LogDto>();
+
+            using (var stream = new MemoryStream())
+            {
+                await formFile.CopyToAsync(stream, cancellationToken);
+
+                using (var package = new ExcelPackage(stream))
+                {
+                    ExcelWorksheet worksheet = package.Workbook.Worksheets[0];
+                    var rowCount = worksheet.Dimension.Rows;
+
+                    for (int row = 2; row <= rowCount; row++)
+                    {
+                        list.Add(new LogDto
+                        {
+                            Name = worksheet.Cells[row, 1].Value.ToString().Trim(),
+                            Surname = worksheet.Cells[row, 2].Value.ToString().Trim(),
+                            MobilNo = worksheet.Cells[row, 3].Value.ToString().Trim(),
+                            BirthDate = worksheet.Cells[row, 4].Value.ToString().Trim(),
+                            LastLocation = worksheet.Cells[row, 5].Value.ToString().Trim(),
+                        });
+                    }
+                }
+            }
+
+            return ExcelResponseDto<List<LogDto>>.GetResult(0, "OK", list);
         }
     }
 }
---
+++ ElasticSearch.Dto/ExcelResponseDto.cs
@@ -0,0 +1,26 @@
+﻿using OfficeOpenXml.FormulaParsing.Excel.Functions.Text;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ElasticSearch.Dto
+{
+    public class ExcelResponseDto<T>
+    {
+        public int Code { get; set; }
+
+        public string Msg { get; set; }
+
+        public T Data { get; set; }
+
+        public static ExcelResponseDto<T> GetResult(int code, string msg, T data = default(T))
+        {
+            return new ExcelResponseDto<T>
+            {
+                Code = code,
+                Msg = msg,
+                Data = data
+            };
+        }
+    }
+}
